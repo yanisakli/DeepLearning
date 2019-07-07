5cc7ff4df89551ea8bb30b7ad1d2b0d92c42a8f4
Yanis <yanisakli@yahoo.fr>
2019-07-07 22:05:49 +0200

    :tada: Printf the result :fire: :fire:

--- venv/DeepLearning.py
+++ venv/DeepLearning.py
@@ -51,3 +51,5 @@ test_image = image.load_img('./dog.jpg',target_size=(64,64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
+print(result)
+training_set.class_indices
