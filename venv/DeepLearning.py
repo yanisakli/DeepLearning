c8d434b2178724163df74ba108d2a52cbdd7c949
Yanis <yanisakli@yahoo.fr>
2019-07-20 20:33:41 +0200

    :tada: tensorboard

--- venv/DeepLearning.py
+++ venv/DeepLearning.py
@@ -3,8 +3,9 @@ from tensorflow.python.keras.layers import *
 from keras.preprocessing import image
 import numpy as np
 import datetime
-
-
+from tensorflow.python.keras.callbacks import TensorBoard
+from time import time
+import tensorflow as tf
 
 classifier = Sequential()
 
@@ -24,6 +25,9 @@ classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accur
 
 from keras.preprocessing.image import ImageDataGenerator
 
+
+tensorboard = TensorBoard(log_dir="/tmp/weka".format(time()))
+
 trainDatagen = ImageDataGenerator(rescale=1. / 255,
                                   shear_range=0.2,
                                   zoom_range=0.2,
@@ -42,10 +46,10 @@ test_set = testDatagen.flow_from_directory('./dataset/test_set',
                                            class_mode='binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch=8,
-                         epochs=2,
-                         validation_data=test_set,
-                         max_queue_size=2000)
+                         steps_per_epoch=5,
+                         epochs=7,
+                         callbacks=[tensorboard],
+                         validation_data=test_set)
 
 test_image = image.load_img('./dog.jpg',target_size=(64,64))
 test_image = image.img_to_array(test_image)
@@ -54,13 +58,16 @@ result = classifier.predict(test_image)
 print(result)
 print(training_set.class_indices)
 
+with tf.compat.v1.Session() as sess :
+    writer = tf.compat.v1.summary.FileWriter('/tmp/weka', sess.graph)
+
 
 now=datetime.datetime.now()
 
 #save the training
-classifier.save(open("Model"+now.isoformat(),"wb"))
+#classifier.save(open("Model"+now.isoformat(),"wb"))
 
 #test the training : load
-#Model = load_model("Model")
+#model = load_model("Model")
 
-#Model.predict(test_image)
+#model.predict(test_image)
